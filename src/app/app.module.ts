93794534266d5bf0dd61cbbe75ba21935c6875a5
Esteban Gehring <esteban.gehring@bithost.ch>
2020-08-04 18:30:06 +0200

    update ngx-mat-select-search to 3.0.2, fix infinite scroll example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectInfiniteScrollModule } from 'ng-mat-select-infinite-scroll';
 
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
@@ -40,7 +41,8 @@ import { InfiniteScrollExampleComponent } from './examples/08-infinite-scroll-ex
     MatToolbarModule,
     MatFormFieldModule,
     MatIconModule,
-    NgxMatSelectSearchModule
+    NgxMatSelectSearchModule,
+    MatSelectInfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
